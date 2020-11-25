cb5895058e1a1ab0cc0af6c0878da0b27e11c9b8
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2020-11-25 20:58:35 +0100

    🎉 Initial commit for the app

---
+++ app/src/main/java/com/codepath_group16/unigram/MainActivity.java
@@ -0,0 +1,14 @@
+package com.codepath_group16.unigram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
