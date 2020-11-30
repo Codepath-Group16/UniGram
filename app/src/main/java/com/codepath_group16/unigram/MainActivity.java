4a92e2750898ffc8fe650a4118d5ffada49aa8cc
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2020-11-30 12:44:33 +0100

    ✨ feat: use view binding in the MainActivity

--- app/src/main/java/com/codepath_group16/unigram/MainActivity.java
+++ app/src/main/java/com/codepath_group16/unigram/MainActivity.java
@@ -2,31 +2,35 @@ package com.codepath_group16.unigram;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.codepath_group16.unigram.databinding.ActivityMainBinding;
+
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_feed, R.id.navigation_explore, R.id.navigation_post, R.id.navigation_profile)
                 .build();
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-        NavController navController = navHostFragment.getNavController();
+        NavController navController = Objects.requireNonNull(navHostFragment).getNavController();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupWithNavController(mBinding.navView, navController);
     }
 
 }
