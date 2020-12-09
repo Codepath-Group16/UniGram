566352053610078cb04a2616899d5a762bfbe8ad
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2020-12-09 07:10:43 +0100

    💄 style: add the appropriate menus

--- app/src/main/java/com/codepath_group16/unigram/ui/post/CompletePostFragment.java
+++ app/src/main/java/com/codepath_group16/unigram/ui/post/CompletePostFragment.java
@@ -3,6 +3,9 @@ package com.codepath_group16.unigram.ui.post;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -11,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
+import com.codepath_group16.unigram.R;
 import com.codepath_group16.unigram.databinding.FragmentCompletePostBinding;
 
 public class CompletePostFragment extends Fragment {
@@ -20,6 +24,7 @@ public class CompletePostFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -41,5 +46,23 @@ public class CompletePostFragment extends Fragment {
                 .load(imageUri)
                 .centerCrop()
                 .into(mBinding.selectedImage);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.complete_post_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_post) {
+            postImage();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void postImage() {
+
     }
 }
