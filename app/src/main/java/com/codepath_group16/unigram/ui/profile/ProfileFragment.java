cb5b08a3987c28ec76ea7eee4aa129168ffc6c1a
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2020-12-12 08:40:37 +0100

    🎉 Begin the profile journey

--- app/src/main/java/com/codepath_group16/unigram/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/codepath_group16/unigram/ui/profile/ProfileFragment.java
@@ -4,15 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.codepath_group16.unigram.R;
+import com.codepath_group16.unigram.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
 
@@ -20,11 +16,8 @@ public class ProfileFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        mProfileViewModel =
-                new ViewModelProvider(this).get(ProfileViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_explore, container, false);
-        final TextView textView = root.findViewById(R.id.text_explore);
-        mProfileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+
+        FragmentProfileBinding binding = FragmentProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 }
